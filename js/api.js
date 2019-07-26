9186a6f04b5918929bedc8ec75a85145af84ca5d
kwekuboateng <fredshavnqweku@gmail.com>
2019-07-26 03:46:23 +0000

    allowed pdf upload convert table to pdf and download clean ui

--- js/api.js
+++ js/api.js
@@ -101,6 +101,8 @@
               events = http.responseText
 
                   localStorage.setItem('events', events)
+                  document.getElementById('loading3').style.display = 'none'
+
                   alert('Successfully created event')
                   window.location.href = "/events.html"
                  
@@ -108,26 +110,50 @@
             alert('Please fill all fields')
           }
       }
-        
-        http.send(params);
-        
+      document.getElementById('loading3').style.display = 'inline-block'
+      http.send(params);  
     }
    
 
+    //fetch single event
+
+    function getSingleEvent(id){
+      var http = new XMLHttpRequest();
+      var loginUrl = `${baseUrl}/api/events/${id}` 
+      ;
+      http.open('GET', loginUrl, true);
+          const jwtoken = JSON.parse(localStorage.getItem('data')).token
+         //Send the proper header information along with the request
+         http.setRequestHeader('Authorization', 'Bearer ' + jwtoken);
   
+         http.onreadystatechange = function() {//Call a function when the state changes.
+          if(http.readyState == 4 && http.status == 200) {
+              userEvent = JSON.parse(http.responseText)
+              document.getElementById('loading').style.display = 'none'
+             if (typeof(Storage) !== 'undefined'){
+              localStorage.setItem('singleEvent', JSON.stringify(userEvent))
+              }
+              console.log(http, 'http')    
+             
+          }
+      }
+      document.getElementById('loading').style.display = 'inline-block'
+        http.send();
+     }
     
     //get all events 
-    var globalCounter = 0;
-    var tbody = document.getElementById('tbody');
 
 
-    function example() {
+    function eventTable() {
      
       var data = JSON.parse(localStorage.getItem('events')); // create object
       console.log(data, 'data')
      
       var table = document.createElement("table");
       table.id = 'dataTable';
+      table.classList.add('table');
+      table.classList.add('table-bordered');
+      table.classList.add('text-center')
   
       var thead = document.createElement("thead");
       table.appendChild(thead);
@@ -140,10 +166,10 @@
   
       var idHeaderCell = document.createElement("th");
       var nameHeader = document.createElement("th");
-      var logoHeader = document.createElement("th");
+      // var logoHeader = document.createElement("th");
       var messageHeader = document.createElement("th");
       var descHeader = document.createElement("th");    
-      var ratingHeader = document.createElement("th");
+      var attendantHeader = document.createElement("th");
       var buttonHeader = document.createElement("th");
       var submitHeader = document.createElement("th");
       var editHeader = document.createElement("th");
@@ -152,10 +178,10 @@
       
      
       nameHeader.innerHTML = 'Name';
-      logoHeader.innerHTML = 'Logo';
+      // logoHeader.innerHTML = 'Logo';
       messageHeader.innerHTML = 'Message';
-      descHeader.innerHTML = 'Description';
-      ratingHeader.innerHTML = 'Rating Message'
+      descHeader.innerHTML = 'View Attendants';
+      attendantHeader.innerHTML = 'No. of Attendants'
       submitHeader.innerHTML = 'Submit Message'
       editHeader.innerHTML = 'Edit Event'
       buttonHeader.innerHTML = 'Attendant Page'
@@ -164,9 +190,9 @@
       header.appendChild(nameHeader);
       header.appendChild(messageHeader);   
       header.appendChild(descHeader);
-      header.appendChild(ratingHeader);
+      header.appendChild(attendantHeader);
       header.appendChild(submitHeader);
-      header.appendChild(logoHeader);
+      // header.appendChild(logoHeader);
       header.appendChild(editHeader);
       header.appendChild(buttonHeader);
    
@@ -183,16 +209,23 @@
           var message = data[i].message;
           var pluLink = data[i].pluLink;
           var submitMessage= data[i].submitMessage
+          var attendant = data[i].attendants;
+          
 
   
           var tr = document.createElement("tr");
   
             //  var idCell = document.createElement("th");
             var nameCell = document.createElement("th");
-            var logoCell = document.createElement("th");
+            // var logoCell = document.createElement("th");
             var messageCell = document.createElement("th");
-            var descCell = document.createElement("th");    
-            var ratingCell = document.createElement("th");
+            var descCell = document.createElement("th");
+            var descbutton = document.createElement("a");
+                descbutton.setAttribute('name', 'view attendant')
+                descbutton.innerHTML = 'view attendants'
+                descbutton.setAttribute('onclick', "window.location.href" + `="/listAttendant.html?id=${id}"`)
+
+            var attendantCell = document.createElement("th");
             var submitCell = document.createElement("th");
             var editCell = document.createElement("th");
             var openCell = document.createElement("th");
@@ -214,19 +247,19 @@
                 
                
 
-          var createImg = document.createElement('img');
-              createImg.src = logoUrl;
-              createImg.width = '100';
-              createImg.height = '70';
+          // var createImg = document.createElement('img');
+          //     createImg.src = logoUrl;
+          //     createImg.width = '100';
+          //     createImg.height = '70';
 
             
           // idCell.appendChild(document.createTextNode(id));
           nameCell.appendChild(document.createTextNode(name));
           messageCell.appendChild(document.createTextNode(message));
-          descCell.appendChild(document.createTextNode(description));
-          ratingCell.appendChild(document.createTextNode(ratingMessage));
+          descCell.appendChild(descbutton);
+          attendantCell.appendChild(document.createTextNode(attendant.length));
           submitCell.appendChild(document.createTextNode(submitMessage));
-          logoCell.appendChild(createImg);
+          // logoCell.appendChild(createImg);
           editCell.appendChild(editbuttonCell);
           openCell.appendChild(buttonCell);
         
@@ -236,16 +269,110 @@
           tr.appendChild(nameCell);
           tr.appendChild(messageCell);
           tr.appendChild(descCell);
-          tr.appendChild(ratingCell);
+          tr.appendChild(attendantCell);
           tr.appendChild(submitCell);
-          tr.appendChild(logoCell);
+          // tr.appendChild(logoCell);
           tr.appendChild(editCell)
           tr.appendChild(openCell);
   
           tbody.appendChild(tr);
       }
-      document.querySelector('#dataTable').appendChild(table);
+      document.querySelector('#eventTab').appendChild(table);
   }
+
+
+  function attendentTable() {
+     
+    var data = JSON.parse(localStorage.getItem('singleEvent')); // create object
+      newData = data.attendants;
+
+      
+    var table = document.createElement("table");
+    table.id = 'dataTable';
+    table.classList.add('table');
+    table.classList.add('table-bordered')
+
+    var thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    var tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+
+    //Add a header
+    var header = document.createElement("tr");
+
+   
+    var nameHeader = document.createElement("th");
+    var emailHeader = document.createElement("th");
+    var phoneHeader = document.createElement("th");    
+    var orgHeader = document.createElement("th");
+    var positionHeader = document.createElement("th");
+    // var pdfHeader = document.createElement("th");
+
+
+    
+   
+    nameHeader.innerHTML = 'Name';
+    emailHeader.innerHTML = 'Email';
+    phoneHeader.innerHTML = 'Phone Number';
+    orgHeader.innerHTML = 'Company'
+    positionHeader.innerHTML = 'Position'
+    // pdfHeader.innerHTML = 'Download PDF'
+
+
+    // header.appendChild(idHeaderCell);
+    header.appendChild(nameHeader);
+    header.appendChild(emailHeader);   
+    header.appendChild(phoneHeader);
+    header.appendChild(orgHeader);
+    header.appendChild(positionHeader);
+  
+ 
+    thead.appendChild(header);
+
+    //Add the rest of the data to the table
+    for (var i = 0; i < newData.length; i++) {
+        // var id = (i + 1);
+      
+        var name = newData[i].name;
+        var email = newData[i].email;
+        var phoneNumber = newData[i].phoneNumber;
+        var position = newData[i].position;
+        var organisation = newData[i].organisation;
+       
+
+
+        var tr = document.createElement("tr");
+
+          //  var idCell = document.createElement("th");
+          var nameCell = document.createElement("th");
+          var emailCell = document.createElement("th");
+          var phoneCell = document.createElement("th");
+          var positionCell = document.createElement("th");    
+          var orgCell = document.createElement("th");
+          
+              
+          
+        // idCell.appendChild(document.createTextNode(id));
+        nameCell.appendChild(document.createTextNode(name));
+        emailCell.appendChild(document.createTextNode(email));
+        phoneCell.appendChild(document.createTextNode(phoneNumber));
+        positionCell.appendChild(document.createTextNode(position));
+        orgCell.appendChild(document.createTextNode(organisation));
+        
+
+
+        // tr.appendChild(idCell);
+        tr.appendChild(nameCell);
+        tr.appendChild(emailCell);
+        tr.appendChild(phoneCell);
+        tr.appendChild(orgCell);
+        tr.appendChild(positionCell);
+
+        tbody.appendChild(tr);
+    }
+    document.querySelector('#eventTab').appendChild(table);
+}
   
 
      //login
@@ -275,16 +402,18 @@
                 if (typeof(Storage) !== 'undefined'){
                     localStorage.setItem('data', data)
                 }
-                window.location.href = '/dashboard.html'
+                window.location.href = '/events.html'
                
                 console.log(http, 'data')
             }else if (http.readyState == 4 && http.status == 400){
-              document.getElementById('loading').innerHTML = ""
+              document.getElementById("loading").style.display = 'none' 
+
                   alert('Please enter correct credentials and try again')
 
             }
         }
-        document.getElementById("loading").innerHTML = '<img src="https://sabarimala.keralartc.com/img/loading.gif" />'
+        document.getElementById("loading").style.display = 'inline-block' 
+
         http.send(params);
      }
 
@@ -318,16 +447,16 @@
                 data = http.responseText
 
                 
-              document.getElementById('loading').innerHTML = ""
-
+              document.getElementById('loading').innerHTML = "none"
+              alert('successfully created account.')
               window.location.href = "/index.html"
                 
                 console.log(http, 'data')
             }else {
-                console.log(http, 'error')
+                alert('ooops. Please fill all fields and try again')
             }
         }
-        document.getElementById("loading").innerHTML = '<img src="https://sabarimala.keralartc.com/img/loading.gif" />'
+        document.getElementById("loading").style.display = 'inline-block' 
         http.send(params);
         
      }
@@ -360,7 +489,7 @@
       http.send();
    }
 
-     //populate dashboard
+   
      
  
   
